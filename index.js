0f5a8edd6d377e05169b569283cc1ff04122f042
JulianImperiale <julian.m.imperiale@gmail.com>
2025-07-30 15:49:35 -0300

    Últimos retoques del portafolio

---
+++ index.js
@@ -0,0 +1,25 @@
+const langBtn = document.getElementById("lang-btn");
+const langIcon = document.getElementById("lang-icon");
+let currentLang = "es";
+
+langBtn.addEventListener("click", () => {
+    currentLang = currentLang === "es" ? "en" : "es";
+    // Cambia el icono según el idioma
+    if (currentLang === "es") {
+        langIcon.src = "images/Iconos/united-states.png";
+        langIcon.alt = "eeuu";
+    } else {
+        langIcon.src = "images/Iconos/spain.png";
+        langIcon.alt = "spain";
+    }
+
+document.querySelectorAll("[data-es]").forEach(el => {
+    // Si el elemento tiene hijos (como <li><img>Texto</li>), solo cambia el nodo de texto
+    if (el.childNodes.length > 1 && el.childNodes[0].nodeType === 1) {
+        // Mantiene el <img> y cambia solo el texto
+        el.childNodes[1].textContent = el.getAttribute(`data-${currentLang}`);
+    } else {
+        el.textContent = el.getAttribute(`data-${currentLang}`);
+    }
+});
+});
